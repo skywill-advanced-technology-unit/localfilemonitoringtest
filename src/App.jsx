abe9c3f9f15d6e129b1ecf1c7a84475546cee2d9
root <root@DESKTOP-NU45MR3>
2023-10-30 18:49:58 +0900

    filemonitoring

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [fileContent, setFileContent] = useState("");
+  const [fileHandle, setFileHandle] = useState(null);
+
+  const readFile = async (handle) => {
+    const file = await handle.getFile();
+    const content = await file.text();
+    return content;
+  };
+
+  const handleFileAccess = async () => {
+    if (window.showOpenFilePicker) {
+      const [selectedFileHandle] = await window.showOpenFilePicker();
+      setFileHandle(selectedFileHandle);
+      const initialContent = await readFile(selectedFileHandle);
+      setFileContent(initialContent);
+    }
+  };
+
+  useEffect(() => {
+    let intervalId;
+
+    if (fileHandle) {
+      intervalId = setInterval(async () => {
+        const updatedContent = await readFile(fileHandle);
+        setFileContent(updatedContent);
+      }, 5000); // 5秒ごとに確認
+    }
+
+    return () => {
+      if (intervalId) {
+        clearInterval(intervalId);
+      }
+    };
+  }, [fileHandle]);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <button onClick={handleFileAccess}>ファイルを選択</button>
+      <pre>{fileContent}</pre>
+    </div>
+  );
 }
 
-export default App
+export default App;
